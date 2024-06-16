87503eb4f4c65f9113ae644beea93cb2ededa061
linhQuag <qlinh280502@gmail.com>
2024-06-16 19:44:40 +0700

    add admin dashboard

--- app/home/views.py
+++ app/home/views.py
@@ -3,6 +3,9 @@
 from flask import render_template
 from flask_login import login_required
 
+from flask import abort
+from flask_login import current_user
+
 from . import home
 
 @home.route('/')
@@ -19,3 +22,13 @@ def dashboard():
     Render the dashboard template on the /dashboard route
     """
     return render_template('home/dashboard.html', title="Dashboard")
+
+# add admin dashboard view
+@home.route('/admin/dashboard')
+@login_required
+def admin_dashboard():
+    # prevent non-admins from accessing the page
+    if not current_user.is_admin:
+        abort(403)
+
+    return render_template('home/admin_dashboard.html', title="Dashboard")
